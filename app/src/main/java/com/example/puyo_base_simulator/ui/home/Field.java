2193dbe113c9a814b1f068a904b4e0914dc2ff84
koiking213 <koiking213@gmail.com>
2020-08-30 16:05:56 +0900

    WIP: 連鎖中のフィールドの持ち方に関するリファクタリング中

--- app/src/main/java/com/example/puyo_base_simulator/ui/home/Field.java
+++ app/src/main/java/com/example/puyo_base_simulator/ui/home/Field.java
@@ -11,6 +11,12 @@ import java.util.Stack;
 public class Field implements Cloneable {
     Puyo[][] field;
     int[] heights = {0,0,0,0,0,0,0};
+    ArrayList<Puyo> disappearPuyo;
+    int accumulatedPoint;
+    int bonus;
+    int colorBonus;
+    int chainNum;
+    int connectionBonus;
     final int[] chainBonusConstant = {0, 0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512};
     final int[] colorBonusConstant = {0, 0, 3, 6, 12};
     int connectionBonusConstant(int connectionNum) {
@@ -56,6 +62,54 @@ public class Field implements Cloneable {
         field[row][column] = new Puyo(row, column, color);
         heights[column]++;
         return true;
+    }
+
+    // todo: evalChainはこっちにして元のevalChainは消す
+    //       現状連鎖しながら描画しているが、先に連鎖結果をすべて保持するようにする
+    List<Field> getFieldTransition() {
+        Field nextField = evalChain(this.chainNum+1, 0).nextField;
+
+        ArrayList<Puyo> disappear = new ArrayList<>();
+        Field newField = new Field();
+        Set<PuyoColor> colors = new HashSet<>();
+        int connectionBonus = 0;
+        // 消えるぷよを探す
+        for (int i=1; i<13; i++) {
+            for (int j=1; j<7; j++) {
+                Puyo puyo = field[i][j];
+                List<Puyo> connection = getConnection(puyo);
+                if (connection.size() >= 4) {
+                    disappear.add(puyo);
+                    // 色ボーナスの評価
+                    colors.add(puyo.color);
+                    // 連結ボーナスの評価
+                    boolean connectionIsNew = true;
+                    for (Puyo p: disappear) {
+                        if (p.row < i || p.row == i && p.column < j) {
+                            connectionIsNew = false;
+                            break;
+                        }
+                    }
+                    if (connectionIsNew) {
+                        connectionBonus += connectionBonusConstant(connection.size());
+                    }
+                } else if (puyo.color != PuyoColor.EMPTY){
+                    newField.addPuyo(j, puyo.color);
+                }
+            }
+        }
+        int bonus = colorBonusConstant[colors.size()] + connectionBonus + chainBonusConstant[chainNum];
+        if (bonus == 0) bonus = 1;
+        int point = accumulatedPoint + bonus * disappear.size() * 10;
+
+        this.colorBonus = colorBonusConstant[colors.size()];
+        this.connectionBonus = connectionBonus;
+        this.bonus = bonus;
+        this.disappearPuyo = disappear;
+        this.accumulatedPoint = point;
+        List<Field> ret = newField.getFieldTransition();
+        ret.add(0, this);
+        return ret;
     }
 
     FieldEvaluation evalChain(int chainNum, int accumulatedPoint) {
