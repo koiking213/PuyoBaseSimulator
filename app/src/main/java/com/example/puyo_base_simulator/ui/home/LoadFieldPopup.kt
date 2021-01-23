f92017803f3c81c4fa2d1d195132a5c87df016a2
koiking213 <koiking213@gmail.com>
2021-01-23 18:59:00 +0900

    LoadFieldPopupをリファクタリング

--- app/src/main/java/com/example/puyo_base_simulator/ui/home/LoadFieldPopup.kt
+++ app/src/main/java/com/example/puyo_base_simulator/ui/home/LoadFieldPopup.kt
@@ -3,7 +3,6 @@ package com.example.puyo_base_simulator.ui.home
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.PopupWindow
@@ -15,10 +14,7 @@ import com.example.puyo_base_simulator.R
 import com.example.puyo_base_simulator.data.AppDatabase
 import com.example.puyo_base_simulator.data.Base
 import com.example.puyo_base_simulator.ui.home.Haipuyo.searchSeedWithPattern
-import com.example.puyo_base_simulator.ui.home.LoadFieldAdapter.FieldSelectedListener
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.function.Function
 import java.util.stream.Collectors
 
 class LoadFieldPopup(private val context: Context) : PopupWindow(context) {
@@ -27,76 +23,79 @@ class LoadFieldPopup(private val context: Context) : PopupWindow(context) {
     private lateinit var listener: (Int, FieldPreview) -> Unit
 
     // should be initialized in Presenter?
-    var mDB: AppDatabase
+    private var mDB: AppDatabase = Room.databaseBuilder(context,
+            AppDatabase::class.java, "database-name")
+            .allowMainThreadQueries()
+            .build()
+
     fun setFieldSelectedListener(fieldSelectedListener: (Int, FieldPreview) -> Unit) {
         listener = fieldSelectedListener
-        loadFieldAdapter!!.setFieldSelectedListener(fieldSelectedListener)
+        loadFieldAdapter.setFieldSelectedListener(fieldSelectedListener)
     }
 
     private fun setupView() {
         @SuppressLint("InflateParams") val view = LayoutInflater.from(context).inflate(R.layout.popup_load, null)
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.setHasFixedSize(true)
-        recyclerView.setLayoutManager(GridLayoutManager(context, 4, GridLayoutManager.VERTICAL, false))
+        recyclerView.layoutManager = GridLayoutManager(context, 4, GridLayoutManager.VERTICAL, false)
         recyclerView.addItemDecoration(DividerItemDecoration(context, GridLayoutManager.VERTICAL))
         recyclerView.addItemDecoration(DividerItemDecoration(context, GridLayoutManager.HORIZONTAL))
         loadFieldAdapter = LoadFieldAdapter(ArrayList())
-        recyclerView.setAdapter(loadFieldAdapter)
+        recyclerView.adapter = loadFieldAdapter
         val searchBySeedButton = view.findViewById<Button>(R.id.searchBySeedButton)
-        searchBySeedButton.setOnClickListener { v: View? ->
+        searchBySeedButton.setOnClickListener {
             val editText = view.findViewById<EditText>(R.id.seedEditTextNumberDecimal)
             try {
                 val seed = editText.text.toString().toInt()
-                if (!(0 <= seed && seed <= 65535)) {
-                    throw NumberFormatException()
-                }
-                val bases = mDB.baseDao().findByHash(seed)
-                val fieldPreviews: MutableList<FieldPreview> = ArrayList()
-                for (base in bases) {
-                    fieldPreviews.add(FieldPreview(base.id, seed, base.field))
-                }
-                loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
-                loadFieldAdapter!!.setFieldSelectedListener(listener)
-                recyclerView.setAdapter(loadFieldAdapter)
+                searchBySeed(seed)
             } catch (e: NumberFormatException) {
-                editText.error = "should enter 0-65535."
+                editText.error = e.message
             }
         }
         val searchByPatternButton = view.findViewById<Button>(R.id.searchByPatternButton)
-        searchByPatternButton.setOnClickListener { v: View? ->
+        searchByPatternButton.setOnClickListener {
             val editText = view.findViewById<EditText>(R.id.patternEditText)
             val str = editText.text.toString()
-            val seeds = searchSeedWithPattern(str)
-            val counter = AtomicInteger()
-            val seedsChunks = seeds.stream().collect(Collectors.groupingBy(Function { it: Int? -> counter.getAndIncrement() / 100 })).values
-            val bases: Collection<Base> = seedsChunks.parallelStream().map { seed: List<Int> -> mDB.baseDao().findByAllHash(seed) }.flatMap { obj: List<Base> -> obj.stream() }.collect(Collectors.toList())
-            val fieldPreviews: MutableList<FieldPreview> = ArrayList()
-            for (base in bases) {
-                fieldPreviews.add(FieldPreview(base.id, base.hash, base.field))
-            }
-            loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
-            loadFieldAdapter!!.setFieldSelectedListener(listener)
-            recyclerView.setAdapter(loadFieldAdapter)
+            searchByPattern(str)
         }
         val showAllButton = view.findViewById<Button>(R.id.showAllButton)
-        showAllButton.setOnClickListener { v: View? ->
-            val bases = mDB.baseDao().all
-            val fieldPreviews: MutableList<FieldPreview> = ArrayList()
-            for (base in bases) {
-                fieldPreviews.add(FieldPreview(base.id, base.hash, base.field))
-            }
-            loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
-            loadFieldAdapter!!.setFieldSelectedListener(listener)
-            recyclerView.setAdapter(loadFieldAdapter)
+        showAllButton.setOnClickListener {
+            showAll()
         }
         contentView = view
     }
 
+    private fun searchBySeed(seed : Int) {
+        if (seed !in 0..65535) {
+            throw NumberFormatException("should enter 0-65535")
+        }
+        val bases = mDB.baseDao().findByHash(seed)
+        val fieldPreviews = bases.map {FieldPreview(it.id, seed, it.field)}
+        loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
+        loadFieldAdapter.setFieldSelectedListener(listener)
+        recyclerView.adapter = loadFieldAdapter
+    }
+
+    private fun searchByPattern(pattern : String) {
+        val seeds = searchSeedWithPattern(pattern)
+        val seedsChunks = seeds.chunked(100)
+        val bases = seedsChunks.parallelStream().map { seed: List<Int> -> mDB.baseDao().findByAllHash(seed) }.flatMap { obj: List<Base> -> obj.stream() }.collect(Collectors.toList())
+        val fieldPreviews = bases.map {FieldPreview(it.id, it.hash, it.field)}
+        loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
+        loadFieldAdapter.setFieldSelectedListener(listener)
+        recyclerView.adapter = loadFieldAdapter
+
+    }
+
+    private fun showAll () {
+        val bases = mDB.baseDao().all
+        val fieldPreviews = bases.map {FieldPreview(it.id, it.hash, it.field)}
+        loadFieldAdapter = LoadFieldAdapter(fieldPreviews)
+        loadFieldAdapter.setFieldSelectedListener(listener)
+        recyclerView.adapter = loadFieldAdapter
+    }
+
     init {
-        mDB = Room.databaseBuilder(context,
-                AppDatabase::class.java, "database-name")
-                .allowMainThreadQueries()
-                .build()
         setupView()
     }
 }
