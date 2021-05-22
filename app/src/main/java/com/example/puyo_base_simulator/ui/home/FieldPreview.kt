3d367901b6f5b126f39866874eefce133993b063
koiking213 <koiking213@gmail.com>
2021-05-22 16:15:50 +0900

    使わなくなったファイルを削除

--- app/src/main/java/com/example/puyo_base_simulator/ui/home/FieldPreview.kt
+++
@@ -1,8 +0,0 @@
-package com.example.puyo_base_simulator.ui.home
-
-data class FieldPreview(val id: Int,
-                        val seed: Int,
-                        val allClear: Boolean,
-                        val numOfPlacement: Int,
-                        val point: Int,
-                        val content: String)
